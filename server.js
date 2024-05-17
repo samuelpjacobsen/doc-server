f2d54e5f8e46f0271acd00b2df68928fcae4f8a8
Samuel Jacobsen <samuel.jacobsen@startse.com>
2024-05-17 10:07:57 -0300

    feat/add-repo

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const cors = require('cors'); 
+
+const app = express();
+const port = 8080;
+
+app.use(cors()); 
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get('/laudo.pdf', (req, res) => {
+  const filePath = path.join(__dirname, 'public', 'laudo.pdf');
+  res.sendFile(filePath);
+});
+
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
